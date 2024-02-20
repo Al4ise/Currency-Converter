feb11188ebd767d558d1a2e3a9c63fd36e6ef650
m.pelteshki24 <m.pelteshki24@LAB108PC11>
2024-02-20 13:35:14 +0200

    Add project files.

---
+++ Currency Converter/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Currency_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double val = 0;
+            double oout = 0;
+            try
+            {
+                val = Convert.ToDouble(textBox1.Text);
+            }
+            catch {
+                label1.Text = "Invalid Input";
+            }
+
+
+            if (radioButton1.Checked)
+            {
+                oout = CurrencyConverter.ConvertToBGN(val);
+            }
+            else
+            {
+                oout = CurrencyConverter.ConvertToEUR(val);
+            }
+
+            label1.Text = Convert.ToString(oout);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Currency Converter/Logic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Converter
+{
+    internal class CurrencyConverter
+    {
+        private static double conversionRate = 0.51129; // Conversion rate from BGN to EUR as of Feb 20, 2024
+
+        public static double ConvertToEUR(double bgn)
+        {
+            return Math.Round(bgn * conversionRate, 2);
+        }
+
+        public static double ConvertToBGN(double eur)
+        {
+            return Math.Round(eur / conversionRate, 2);
+        }
+    }
+}
